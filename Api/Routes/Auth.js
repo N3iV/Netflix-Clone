9ac962500940ad0f8c6ab41d34f75aa1a00345b8
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2021-12-17 20:03:41 +0700

    implement Login with JWT

--- Api/Routes/Auth.js
+++ Api/Routes/Auth.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const User = require("../Models/User");
 const CryptoJS = require("crypto-js");
-
+const jwt = require("jsonwebtoken");
 //REGISTER
 
 router.post("/register", async (req, res) => {
@@ -21,5 +21,24 @@ router.post("/register", async (req, res) => {
     res.status(500).json(error);
   }
 });
-
+//* LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password and username!");
+    var bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    var originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password and username!");
+    const accessToken = jwt.sign(
+      { id: user.id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+    const { password, ...info } = user._doc;
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
