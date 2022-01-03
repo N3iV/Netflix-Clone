7ff29ed0a9eef86c540f75a9fcf78d6a2d9027d4
Hoang Vien <80262438+N3iV@users.noreply.github.com>
2022-01-03 17:27:05 +0700

    fix ui

--- Client/src/components/ListItem/ListItem.jsx
+++ Client/src/components/ListItem/ListItem.jsx
@@ -1,24 +1,48 @@
-import { PlayArrowRounded } from "@material-ui/icons";
 import { useState } from "react";
+import {
+  PlayArrow,
+  Add,
+  ThumbUpAltOutlined,
+  ThumbDownOutlined,
+} from "@material-ui/icons";
 import "./listItem.scss";
-export default function ListItem() {
-  const [isHover, setIsHover] = useState(false);
-  console.log(isHover);
+export default function ListItem({ index }) {
+  const [isHovered, setIsHovered] = useState(false);
+  console.log(isHovered);
   return (
     <>
       <div
         className="listItem"
-        onMouseOver={() => setIsHover(true)}
-        onMouseLeave={() => setIsHover(false)}
+        style={{ left: isHovered && index * 225 - 50 + index * 2.5 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        <div className={isHover ? `listItem-desc active` : "listItem-desc"}>
-          <PlayArrowRounded class="play" />
-          <span className="listItem-title">Chàng vợ của em</span>
-        </div>
-        <img
-          src="https://occ-0-58-325.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABalT23sYUu4RoXoz_3Dl-zRAOJHWekebi4NbUWk57WRJp90TLvPx67pITYV1DEmkzcrqX6hn2HeX_rzkPLanWmdRTUk.jpg?r=46c"
-          alt=""
-        />
+        <img src="" alt="" />
+        {isHovered && (
+          <>
+            <video src="" autoPlay={true} loop />
+            <div className="itemInfo">
+              <div className="icons">
+                <PlayArrow className="icon" />
+                <Add className="icon" />
+                <ThumbUpAltOutlined className="icon" />
+                <ThumbDownOutlined className="icon" />
+              </div>
+              <div className="itemInfoTop">
+                <span>1231231</span>
+                <span className="limit">+4</span>
+                <span>2002</span>
+              </div>
+              <div className="desc">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minus
+                dicta corporis culpa fuga officia aut nisi ab, quae eos vero
+                veritatis omnis voluptate ex dignissimos aliquam nemo. Maxime,
+                officiis labore.
+              </div>
+              <div className="genre">Horror</div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
